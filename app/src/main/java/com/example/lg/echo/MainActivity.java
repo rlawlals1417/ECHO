ce21754506bf2ecec491935f5ad9b947d92feb2b
쥐미니\LG <jmeef0802@gmail.com>
2018-01-21 21:21:26 +0900

    Fragment_tab

---
+++ app/src/main/java/com/example/lg/echo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.lg.echo;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tl_tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_pager);
+
+        Fragment[] arrFragments = new Fragment[3];
+        arrFragments[0] = new RedFragment();
+        arrFragments[1] = new YellowFragment();
+        arrFragments[2] = new GreenFragment();
+
+
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), arrFragments);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private class MyPagerAdapter extends FragmentPagerAdapter{
+        private Fragment[] arrFragments;
+        public MyPagerAdapter(FragmentManager fm, Fragment[] arrFragments) {
+            super(fm);
+            this.arrFragments = arrFragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return arrFragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return arrFragments.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch(position){
+                case 0:
+                    return "RED";
+                case 1:
+                    return "YELLOW";
+                case 2:
+                    return "GREEN";
+                default:
+                    return "";
+            }
+
+
+
+        }
+    }
+}
